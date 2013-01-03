61dc8158e4d6a04652984540e1603137c43c6e05
Eric Adams <eadamse@gmail.com>
2013-01-03 13:50:55 -0600

    base install of brewhub rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewhub::Application.config.secret_token = '4a078431b95de14c5466db0889914f7b8215745a16a29b3be02a10243a4a70f38015f145d6dbd671395d0333c068824e5dee587fccb0ff23b6a21b99d030ab7c'
